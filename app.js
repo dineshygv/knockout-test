e4307d3a8ecd177de5ec99e72a02f0249181a843
Dinesh Yenneti <goyennet@microsoft.com>
2016-09-09 21:02:37 +0800

    Added page.js router

--- app.js
+++ app.js
@@ -1,15 +1,44 @@
-
+var page = require("page");
 var ko = require("knockout");
 var $ = require("jquery");
 var loginPage = require("./login-page");
 
-var pageParams = {
+/**
+ * Bootstrap needs jQuery exposed as global variable.
+ * Webpack-expose loader exposes $ and jquery as globals
+ * https://github.com/gowravshekar/bootstrap-webpack
+ */
+require('expose?$!expose?jQuery!jquery');
+var boostrap = require("bootstrap");
+
+/**
+ * Setting base to /# is required to add hash param to URL
+ * Without this, page.js directly sets page URL which casuses
+ * a 404 when the page is reloaded
+ */
+page.base('/#');
+
+/**
+ * These are default route settings. * 
+ * routeSettings.page should match the name of the page component.
+ * routeSettings.pageParams are additional params obtained rom URL to 
+ * pass to the page component.
+ */
+var routeSettings = {
     page : 'login-page',
-    pageParams : {
-        userName : "Ludo"
-    }    
+    pageParams : {}    
 };
 
+/**
+ * Home page is the login-page.
+ * TODO: Move route config to its own config file
+ */
+page('/', function(){
+    routeSettings.page = "login-page";
+    routeSettings.pageParams = {};
+});
+
 $(function() {
-    ko.applyBindings(pageParams, document.getElementById("container"));
+    page.start();
+    ko.applyBindings(routeSettings, document.getElementById("container"));
 });
--- login-page.js
+++ login-page.js
@@ -2,8 +2,8 @@ var ko = require("knockout");
 var template = require("raw!./login-page.html");
 
 function LoginPageModel(params){
-    this.email = "Email";
-    this.userName = " Hi, " + params.userName;
+    this.email = " Email";
+    this.userName = "Login";
 }
 
 ko.components.register('login-page', {
